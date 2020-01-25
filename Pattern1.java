6d7a983de6d0495665fc3523f55f683ede758404
Khushank <47456301+Khushank2001@users.noreply.github.com>
2020-01-25 14:42:59 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Pattern1 {
 
 	public static void main(String[] args) {
-		
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the value for n");
-	int n = sc.nextInt();
-	
-	for(int j = 1; j <= n ; j++) {
-	
-		for(int i=1; i <=n; i++) {
-		System.out.print("*  ");
 
+		                                            // 		import Scanner class for taking output from user
+	Scanner sc = new Scanner(System.in);            
+	
+		System.out.println("Enter the value for n");
+	
+		int n = sc.nextInt();
+
+// 		using nested for-loops 
+		
+		for(int j = 1; j <= n ; j++) { 
+			for(int i=1; i <=n; i++) {
+				System.out.print("*  ");
 	
 	}
 	
@@ -22,6 +25,7 @@ public class Pattern1 {
 	
 		
 }
+// 	output
 //	* * * *  
 //	* * * *  
 //	* * * *
