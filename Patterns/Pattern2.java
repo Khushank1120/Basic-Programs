462228b5d7bd6a4afafe00cc91fe5f8428c5a8a6
Khushank <47456301+Khushank2001@users.noreply.github.com>
2020-02-14 23:46:48 +0530

    Update Pattern2.java

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Pattern2 {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
+
+		
+		Scanner sc = new Scanner(System.in);     // 	using scanner class and taking input
+		
 		System.out.println("Entre value for n");
 		int n = sc.nextInt();
+
+		// 		using nested for-loop
 		
-		for(int i=1;i<=n;i++) {
+		for(int i=1;i<=n;i++) { 
 		
 			for(int j=1; j<=i; j++) {
 			System.out.print("* ");
@@ -23,6 +27,8 @@ public class Pattern2 {
 	}
 
 }
+
+// output
 //* 
 //* * 
 //* * * 
